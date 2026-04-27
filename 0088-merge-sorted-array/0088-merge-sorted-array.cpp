3d6ced7b115ca2fcd1e58d115e6051b420dacf96
Sneha <2422325.iot.cec@cgc.edu.in>
2026-04-27 16:24:51 +0530

    Time: 0 ms (100%), Space: 12.4 MB (40.52%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+int i = m-1;
+int j = n-1;
+int k = m+n-1;
+while(i>=0 && j>=0){
+    if(nums1[i]<nums2[j]){
+   nums1[k] =nums2[j];
+    j--;
+
+    }
+    else if(nums1[i]>nums2[j]){
+     nums1[k]= nums1[i];
+     i--;
+     
+    }
+    else{
+        nums1[k]=nums2[j];
+        j--;
+    }
+    k--;
+}
+ while(j >= 0){
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+
+
+    }
+};
+
+// LeetCode gives nums1 → you modify it → LeetCode checks it....... no need to run as function is void 
