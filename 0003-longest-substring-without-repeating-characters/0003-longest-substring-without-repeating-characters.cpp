56ca00999b5e799e0612236a953646634d6a4a37
Sneha <2422325.iot.cec@cgc.edu.in>
2026-05-05 13:00:31 +0530

    Time: 19 ms (28.58%), Space: 14.4 MB (21.13%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,22 +1,49 @@
-class Solution {
-public:
-    int lengthOfLongestSubstring(string s) {
-unordered_map<char, int>mp;
-int i =0;
-int j=0;
-int maxlen=0;
-for(j=0; j<s.length(); j++){
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+// unordered_map<char, int>mp;
+// int i =0;
+// int j=0;
+// int maxlen=0;
+// for(j=0; j<s.length(); j++){
   
 
-    if(mp.find(s[j])!=mp.end()){
-        i = max(i, mp[s[j]]+1);
-    }
-    mp[s[j]]=j;
-      int currentlen = j-i+1; // will use this formula in sliding window ,substrings etc problems
-maxlen = max(maxlen, currentlen);
-}
+//     if(mp.find(s[j])!=mp.end()){
+//         i = max(i, mp[s[j]]+1);
+//     }
+//     mp[s[j]]=j;
+//       int currentlen = j-i+1; // will use this formula in sliding window ,substrings etc problems
+// maxlen = max(maxlen, currentlen);
+// }
 
     
-return maxlen;
+// return maxlen;
+//     }
+// };
+
+
+class Solution {
+public:   
+ int lengthOfLongestSubstring(string s) {
+    unordered_set<char>st;
+    int j =0;
+    int i=0;
+    int maxlen=0;
+for(int j =0; j<s.size(); j++){
+   
+    while(st.find(s[j])!=st.end()){
+st.erase(s[i]);
+i++;
+  
     }
+      st.insert(s[j]);
+   
+   
+    maxlen= max(maxlen, j-i+1);
+}
+return maxlen;
+   
+
+ }
 };
+// this is a substring problem., its complexity is O(n) as  i moves when j stays 
