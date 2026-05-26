f2e4a43f6bb3721a7ec0166859376db3f4783729
Sneha <2422325.iot.cec@cgc.edu.in>
2026-05-26 22:52:16 +0530

    Time: 16 ms (59.25%), Space: 26.2 MB (38.25%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    unordered_map<string, vector<string>>mp;
+    
+vector<vector<string>> ans;
+      string word;
+       string sortedword;
+     for(int i =0; i<strs.size();  i++){
+        word = strs[i];
+        sortedword= word;
+       sort(sortedword.begin(), sortedword.end());
+
+    
+mp[sortedword].push_back(word);
+    
+     }
+
+
+for(auto it : mp) {
+    ans.push_back(it.second);
+}
+
+return ans;
+     }        
+    
+};
+// this is of the type hashmap and canonical representation, which means that we will give one standard form to equivalent things, which are anagrams here 
+
+// so if we talk about sorting , so from the whole vector, we will separate it into words and then sort every word then compare 
