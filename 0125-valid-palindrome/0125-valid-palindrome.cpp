c4400d751c6bcc7c45ccf02e43fb1964c60d2e05
Sneha <2422325.iot.cec@cgc.edu.in>
2026-07-22 20:46:13 +0530

    Time: 0 ms (100%), Space: 10.1 MB (37.42%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,25 +1,67 @@
+//optimal approach = two pointers
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string result; // modifying the string that it only contains lower and alphanumeric characters 
-        for(char ch : s){
-            if(isalnum(ch)){
-                result+=tolower(ch);
-            }
-        }
-        int i =0;
-        int  j = result.length()-1;
-     
-        for(i =0; i<=j; i++){
-            if(result[i]==result[j]){
-                j--;
-            }
-            else{
-                return false;
-            }
-            
-        }
-        return true;
+    
+    int i =0; 
+    int j = s.length()-1;
+    while(i<j){
+       if(!isalnum(s[i])){
+        i++;
+       } 
+       else if(!isalnum(s[j])){
+        j--;
+       }
+       else if(tolower(s[i])!=tolower(s[j])){
+return false;
+       }else{
 
+       
+       i++;
+       j--;
+       }
     }
+    
+
+return true;
+        }
+
 };
+
+
+//greedy approach
+// mow the thing is time complexity is still O(n) but space for that result var and extra loop is making it less optimal otherwise in this also we are correctly using the concept of two pointers 
+// class Solution {
+// public:
+//     bool isPalindrome(string s) {
+//         string result; // modifying the string that it only contains lower and alphanumeric characters 
+//         for(char ch : s){
+//             if(isalnum(ch)){
+//                 result+=tolower(ch);
+//             }
+//         }
+//         int i =0;
+//         int  j = result.length()-1;
+     
+//         for(i =0; i<=j; i++){
+//             if(result[i]==result[j]){
+//                 j--;
+//             }
+//             else{
+//                 return false;
+//             }
+            
+//         }
+//         return true;
+
+//     }
+// };
+
+
+
+
+
+
+
+
+
