cc93e15968ee9a21a85ca7393dd99ecb8990ab9b
Sneha <2422325.iot.cec@cgc.edu.in>
2026-09-23 03:41:20 +0530

    Time: 0 ms (100%), Space: 10.2 MB (22.5%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,67 +1,26 @@
-//optimal approach = two pointers
 class Solution {
 public:
     bool isPalindrome(string s) {
-    
-    int i =0; 
-    int j = s.length()-1;
-    while(i<j){
-       if(!isalnum(s[i])){
-        i++;
-       } 
-       else if(!isalnum(s[j])){
-        j--;
-       }
-       else if(tolower(s[i])!=tolower(s[j])){
-return false;
-       }else{
+        string ans;
 
-       
-       i++;
-       j--;
-       }
-    }
-    
-
-return true;
+        for(char c : s) {
+            if(isalnum(c)) {
+                ans.push_back(tolower(c));
+            }
         }
 
+        int i = 0;
+        int j = ans.length() - 1;
+
+        while(i < j) {
+            if(ans[i] != ans[j]) {
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
 };
-
-
-//greedy approach
-// mow the thing is time complexity is still O(n) but space for that result var and extra loop is making it less optimal otherwise in this also we are correctly using the concept of two pointers 
-// class Solution {
-// public:
-//     bool isPalindrome(string s) {
-//         string result; // modifying the string that it only contains lower and alphanumeric characters 
-//         for(char ch : s){
-//             if(isalnum(ch)){
-//                 result+=tolower(ch);
-//             }
-//         }
-//         int i =0;
-//         int  j = result.length()-1;
-     
-//         for(i =0; i<=j; i++){
-//             if(result[i]==result[j]){
-//                 j--;
-//             }
-//             else{
-//                 return false;
-//             }
-            
-//         }
-//         return true;
-
-//     }
-// };
-
-
-
-
-
-
-
-
-
